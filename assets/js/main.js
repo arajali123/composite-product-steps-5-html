3aa93dbd5a5fbfd6387d8b674bc6e0c6fd2bc1c9
arajali123 <arajali.sds@gmail.com>
2025-09-09 18:20:16 +0600

    responsive continure

--- assets/js/main.js
+++ assets/js/main.js
@@ -12,6 +12,31 @@ document.addEventListener("DOMContentLoaded", function () {
   });
 });
 
+// Sidebar Js
+document.addEventListener("DOMContentLoaded", function () {
+  const toggleBtn = document.querySelector(".toggle-btn");
+
+  toggleBtn.addEventListener("click", function () {
+    // current active tab khujbo
+    const activeTab = document.querySelector(".tabs-cards-main-wrap.active");
+    if (!activeTab) return;
+
+    // active tab er sidebar khujbo
+    const sidebar = activeTab.querySelector(".composite-product-cards-sidebar");
+    if (sidebar) {
+      sidebar.classList.add("active");
+
+      // close button handle
+      const closeBtn = sidebar.querySelector(".close-btn");
+      if (closeBtn) {
+        closeBtn.addEventListener("click", function () {
+          sidebar.classList.remove("active");
+        });
+      }
+    }
+  });
+});
+
 // Cart Tabs Js
 document.addEventListener("DOMContentLoaded", function () {
   const tabLinks = document.querySelectorAll(".composite-product-cards-menu .tab-button");
