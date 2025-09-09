912bca041a24776f292491ea9e8b975990c7795b
arajali123 <arajali.sds@gmail.com>
2025-09-09 14:58:13 +0600

    cart button working

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,5 +1,4 @@
 
-// Tabs JS
 // Header Sticky Js
 document.addEventListener("DOMContentLoaded", function () {
   var header = document.querySelector(".header-sticky");
@@ -13,31 +12,7 @@ document.addEventListener("DOMContentLoaded", function () {
   });
 });
 
-// document.addEventListener("DOMContentLoaded", function () {
-//   const tabLinks = document.querySelectorAll(".composite-product-cards-menu .tab-button");
-//   const tabContents = document.querySelectorAll(".tabs-cards-main-wrap");
-
-//   tabLinks.forEach(tabLink => {
-//     tabLink.addEventListener("click", function(e) {
-//       e.preventDefault();
-//       e.stopPropagation();
-
-//       const targetId = this.getAttribute("href");
-//       const targetContent = document.querySelector(targetId);
-
-//       // Remove Tab
-//       tabContents.forEach(c => c.classList.remove("active"));
-
-//       // Show Tab
-//       if (targetContent) {
-//         targetContent.classList.add("active");
-//       }
-
-//       this.classList.toggle("active");
-//     });
-//   });
-// });
-
+// Cart Tabs Js
 document.addEventListener("DOMContentLoaded", function () {
   const tabLinks = document.querySelectorAll(".composite-product-cards-menu .tab-button");
   const tabContents = document.querySelectorAll(".tabs-cards-main-wrap");
@@ -73,6 +48,29 @@ document.addEventListener("DOMContentLoaded", function () {
     });
   });
 });
+
+// Counter Js
+document.querySelectorAll('.counter-cart-box').forEach(counter => {
+  const minus = counter.querySelector('.minus');
+  const plus = counter.querySelector('.plus');
+  const number = counter.querySelector('.counter-number');
+  let count = parseInt(number.textContent);
+
+  minus.addEventListener('click', () => {
+    if (count > 1) {
+      count--;
+      number.textContent = count;
+    }
+  });
+
+  plus.addEventListener('click', () => {
+    count++;
+    number.textContent = count;
+  });
+});
+
+
+
 
 
 
